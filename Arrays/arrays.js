e86955259e3913433125290f897def2a459c17ab
Hamra <hamra.khan.b@gmail.com>
2023-02-18 17:54:41 +0000

    Added unit tests for arrays.js using jest matchers

---
+++ Arrays/arrays.js
@@ -0,0 +1,17 @@
+function shouldEqualTo(arr1) {
+    return arr1;
+}
+
+function fruits(fruits) {
+    return fruits;
+}
+
+function colors(colors) {
+    return colors;
+}
+    
+module.exports = {
+    shouldEqualTo,
+    fruits,
+    colors
+}
